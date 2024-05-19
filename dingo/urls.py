5fc7aedb833c9c66857415380a17a16cac14880f
BartoszOw <owbartosz.pl@gmail.com>
2024-05-19 19:22:11 +0200

    Session/Models

--- dingo/urls.py
+++ dingo/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('maths/', include('maths.urls')),
-    path('greetings/', include('greetings.urls'))
+    path('greetings/', include('greetings.urls')),
+    path('sessions/', include('sessions.urls'))
 ]
--- maths/models.py
+++ maths/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+OPERATION_CHOICES = (
+    ('add', 'add'),
+    ('sub', 'sub'),
+    ('mul', 'mul'),
+    ('div', 'div'),
+)
+class Math(models.Model):
+    operation = models.CharField(max_length=5, choices=OPERATION_CHOICES)
+    a = models.IntegerField()
+    b = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    result = models.ForeignKey(
+        'maths.result',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+        )
+    
+class Result(models.Model):
+    value = models.FloatField(blank=True,null=True, unique=True)
+    error = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        unique=True
+    )
