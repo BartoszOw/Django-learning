7cab8d0bcb2b626ed5212aaa3ea17a3e43e7e5e4
BartoszOw <owbartosz.pl@gmail.com>
2024-05-29 11:10:06 +0200

    AuthorTestView

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -15,3 +15,17 @@ class PostViewTest(TestCase):
         self.assertEqual(len(response.context['posts']), 1)
         expected_link = f'<li><a href="/posts/list/{self.post.id}">{self.post.title}</a></li>'
         self.assertIn(expected_link, response.content.decode())
+
+
+class AuthorViewTest(TestCase):
+    
+    def setUp(self):
+        self.author = Author.objects.create(nick='xxx', email='x@x.pl')
+        self.client = Client()
+
+    def test_post_view(self):
+        response = self.client.get('/posts/authors/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['authors']), 1)
+        expected_link = f'<li><a href="/posts/authors/{self.author.id}">{self.author.nick} {self.author.email}</a></li>'
+        self.assertIn(expected_link, response.content.decode())
